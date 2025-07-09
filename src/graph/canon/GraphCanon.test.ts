310e92f2400f2a925fd29706c1426bba77a4ece7
Marcel Friedrichs <mfriedrichs@techfak.uni-bielefeld.de>
2025-07-09 11:54:11 +0200

    Add unit tests for graph canon

--- src/graph/canon/GraphCanon.test.ts
+++ src/graph/canon/GraphCanon.test.ts
@@ -17,18 +17,34 @@ function getAllPermutations<T>(array: T[]): T[][] {
 	return result;
 }
 
-function applyPermutationToMatrix(
-	matrix: number[][],
-	perm: number[]
-): number[][] {
-	const n = matrix.length;
-	const permuted = Array.from({length: n}, () => Array(n).fill(0));
+function applyPermutationToGraph(graph: Graph, perm: number[]): Graph {
+	const n = graph.adjacencyMatrix.length;
+	const result: Graph = {
+		adjacencyMatrix: Array.from({length: n}, () => Array(n).fill(0)),
+		edgeLabels: graph.edgeLabels
+			? Array.from({length: n}, () => Array(n).fill(''))
+			: undefined,
+		labels: graph.labels ? new Array(n).fill('') : undefined,
+	};
 	for (let i = 0; i < n; i++) {
+		if (graph.labels) {
+			result.labels![perm[i]] = graph.labels[i];
+		}
 		for (let j = 0; j < n; j++) {
-			permuted[i][j] = matrix[perm[i]][perm[j]];
+			result.adjacencyMatrix[perm[i]][perm[j]] = graph.adjacencyMatrix[i][j];
+			if (graph.edgeLabels) {
+				result.edgeLabels![perm[i]][perm[j]] = graph.edgeLabels[i][j];
+			}
 		}
 	}
-	return permuted;
+	return result;
+}
+
+function testAllPermutations(graph: Graph) {
+	testPermutations(
+		graph,
+		getAllPermutations([...Array(graph.adjacencyMatrix.length).keys()])
+	);
 }
 
 function testPermutations(graph: Graph, permutations: number[][]) {
@@ -36,9 +52,7 @@ function testPermutations(graph: Graph, permutations: number[][]) {
 	const canonGraph = canon.canonicalize();
 	const canonGraphString = canon.buildGraphString(canonGraph);
 	permutations.forEach((perm) => {
-		const permGraph: Graph = {
-			adjacencyMatrix: applyPermutationToMatrix(graph.adjacencyMatrix, perm),
-		};
+		const permGraph = applyPermutationToGraph(graph, perm);
 		const permCanon = new GraphCanon(permGraph);
 		const canonGraph = permCanon.canonicalize();
 		expect(permCanon.buildGraphString(canonGraph)).toBe(canonGraphString);
@@ -54,10 +68,50 @@ test('undirected unlabeled graph canon', () => {
 			[0, 1, 1, 0],
 		],
 	};
-	testPermutations(
-		graph,
-		getAllPermutations([...Array(graph.adjacencyMatrix.length).keys()])
-	);
+	testAllPermutations(graph);
+});
+
+test('directed unlabeled graph canon', () => {
+	const graph: Graph = {
+		adjacencyMatrix: [
+			[0, 1, 0, 0],
+			[0, 0, 1, 1],
+			[0, 1, 0, 1],
+			[0, 1, 1, 0],
+		],
+	};
+	testAllPermutations(graph);
+});
+
+test('undirected edge-labeled graph canon', () => {
+	testAllPermutations({
+		adjacencyMatrix: [
+			[0, 1, 0, 0],
+			[1, 0, 1, 1],
+			[0, 1, 0, 1],
+			[0, 1, 1, 0],
+		],
+		edgeLabels: [
+			['', '-', '', ''],
+			['-', '', '=', '='],
+			['', '=', '', '-'],
+			['', '=', '-', ''],
+		],
+	});
+	testAllPermutations({
+		adjacencyMatrix: [
+			[0, 1, 0, 0],
+			[1, 0, 1, 1],
+			[0, 1, 0, 1],
+			[0, 1, 1, 0],
+		],
+		edgeLabels: [
+			['', '-', '', ''],
+			['-', '', '#', '='],
+			['', '#', '', '-'],
+			['', '=', '-', ''],
+		],
+	});
 });
 
 test('larger undirected unlabeled graph canon', () => {
@@ -97,4 +151,58 @@ test('larger undirected unlabeled graph canon', () => {
 		[2, 6, 4, 5, 1, 0, 3, 9, 7, 8],
 		[7, 8, 0, 3, 9, 4, 6, 2, 5, 1],
 	]);
+});
+
+test('undirected unlabeled Petersen graph canon', () => {
+	const graph: Graph = {
+		adjacencyMatrix: [
+			[0, 1, 0, 0, 1, 1, 0, 0, 0, 0],
+			[1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+			[0, 1, 0, 1, 0, 0, 0, 1, 0, 0],
+			[0, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+			[1, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+			[1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+			[0, 1, 0, 0, 0, 1, 0, 1, 0, 0],
+			[0, 0, 1, 0, 0, 0, 1, 0, 1, 0],
+			[0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+			[0, 0, 0, 0, 1, 1, 0, 0, 1, 0],
+		],
+	};
+	testPermutations(graph, [
+		[8, 1, 3, 0, 4, 2, 5, 6, 9, 7],
+		[9, 3, 6, 1, 0, 5, 2, 7, 8, 4],
+		[6, 8, 9, 3, 2, 0, 5, 1, 4, 7],
+		[6, 1, 3, 7, 9, 2, 8, 5, 4, 0],
+		[9, 2, 4, 7, 3, 0, 1, 6, 5, 8],
+		[1, 6, 2, 4, 8, 7, 0, 9, 5, 3],
+		[4, 8, 5, 3, 1, 9, 7, 0, 6, 2],
+		[6, 1, 3, 7, 2, 4, 8, 9, 0, 5],
+		[1, 9, 4, 7, 3, 8, 6, 2, 5, 0],
+		[5, 0, 6, 3, 9, 8, 1, 7, 4, 2],
+		[5, 1, 6, 0, 7, 9, 8, 3, 4, 2],
+		[3, 9, 7, 8, 0, 5, 6, 4, 1, 2],
+		[5, 9, 7, 3, 2, 0, 6, 1, 8, 4],
+		[9, 2, 4, 0, 7, 3, 5, 8, 1, 6],
+		[3, 2, 0, 7, 6, 9, 1, 4, 5, 8],
+		[3, 5, 2, 8, 7, 9, 6, 0, 4, 1],
+		[9, 7, 8, 2, 3, 5, 6, 1, 4, 0],
+		[6, 7, 1, 3, 5, 4, 0, 8, 9, 2],
+		[2, 6, 4, 5, 1, 0, 3, 9, 7, 8],
+		[7, 8, 0, 3, 9, 4, 6, 2, 5, 1],
+	]);
+});
+
+test('undirected node-labeled graph canon', () => {
+	const graph: Graph = {
+		adjacencyMatrix: [
+			[0, 1, 1, 0, 0, 0],
+			[1, 0, 0, 1, 0, 0],
+			[1, 0, 0, 1, 1, 0],
+			[0, 1, 1, 0, 0, 1],
+			[0, 0, 1, 0, 0, 1],
+			[0, 0, 0, 1, 1, 0],
+		],
+		labels: ['A', 'B', 'B', 'A', 'C', 'C'],
+	};
+	testAllPermutations(graph);
 });
